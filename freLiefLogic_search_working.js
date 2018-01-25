7627b083bb88fd1911c90e491696ff069634188c
Steffen Sornpao <ssornpao1@steffens-MacBook-Pro.local>
2018-01-24 19:06:51 -0500

    search auto populate

--- freLiefLogic_search_working.js
+++ freLiefLogic_search_working.js
@@ -11,17 +11,39 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
+var search = $("#categories").val();
+
+//Auto populate
+database.ref().orderByChild(search).limitToLast(10).on("child_added", function(childSnapshot) {
+    contactName = childSnapshot.val().contactName;
+    nameOfOrganization = childSnapshot.val().nameOfOrganization;
+    servicesOffered = childSnapshot.val().servicesOffered;
+    address = childSnapshot.val().address;
+    city = childSnapshot.val().city;
+    state = childSnapshot.val().state;
+    zip = childSnapshot.val().zip;
+    phonenum = childSnapshot.val().phonenum;
+
+ $("#post-container").append(
+   "<div class='post'>"+
+     "<div class='img'></div>"+
+     "<div class='data'>"+
+       "<p>" + servicesOffered + "</p>"+
+       "<p>" + contactName + "</p>"+
+       "<p>" + address + "</p>"+
+       "</div></div></div>"
+     )
+   });
+
 //Click search button
 $("#search-button").on("click", function(event) {
  event.preventDefault();
  $("#post-container").html(" ");
-
- console.log($("#categories").val())
- search = $("#categories").val();
+ var search = $("#categories").val();
 
  database.ref().orderByChild(search).limitToLast(10).on("child_added", function(childSnapshot) {
    if (search == childSnapshot.val().servicesOffered){
-     contactName        = childSnapshot.val().contactName;
+     contactName = childSnapshot.val().contactName;
      nameOfOrganization = childSnapshot.val().nameOfOrganization;
      servicesOffered = childSnapshot.val().servicesOffered;
      address = childSnapshot.val().address;
